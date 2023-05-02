e45c25953b39918fae5e97b23c611f2266630001
e11it <im@e11it.ru>
2023-05-02 20:07:03 +0300

    Add SimplePrincipal type

--- pydantic_common_models/kafka/simple_acl.py
+++ pydantic_common_models/kafka/simple_acl.py
@@ -1,6 +1,51 @@
 # Kafka Simple Authorizer data model
 from enum import StrEnum
+import re
 
+re_principal = r"^User:(?P<user>\S+?)$"
+re_principal_comp = re.compile(re_principal, re.IGNORECASE)
+
+
+class SimplePrincipal(str):
+    """
+    Kafka Simple Acl principal name
+    """
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        # __modify_schema__ should mutate the dict it receives in place,
+        # the returned value will be ignored
+        field_schema.update(
+            # simplified regex here for brevity, see the wikipedia link above
+            pattern=re_principal,
+            # some example postcodes
+            examples=['User:some_user_name'],
+        )
+
+    @classmethod
+    def __get_validators__(cls):
+        # one or more validators may be yielded which will be called in the
+        # order to validate the input, each validator will receive as an input
+        # the value returned from the previous validator
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not isinstance(v, str):
+            raise TypeError('string required')
+
+        return cls(v)
+
+    def __init__(self, principal):
+        match = re_principal_comp.fullmatch(principal)
+        if not match:
+            raise ValueError('invalid principal name')
+
+        self._user = match.group("user")
+
+    @property
+    def user(self):
+        return self._user
 
 class _EnumCaseInsensitive:
     """_missing_ method override to make Enums CaseInsensitive"""
--- pydantic_common_models/ksm/csv_acl.py
+++ pydantic_common_models/ksm/csv_acl.py
@@ -6,7 +6,7 @@ from pydantic_common_models.kafka.simple_acl import \
     PermissionType, \
     KafkaTopicOperations, \
     KafkaResourcePatternType, \
-    Resources
+    Resources, SimplePrincipal
 
 #
 KSM_CSV_HEADERS = ["KafkaPrincipal",
@@ -28,7 +28,7 @@ class KafkaAclCSVGetter(GetterDict):
 
 
 class KafkaACL(BaseModel):
-    kafka_principal: str = Field(..., alias="KafkaPrincipal",  regex=r'^User:[a-zA-Z0-9_@\.-]+$')
+    kafka_principal: SimplePrincipal = Field(..., alias="KafkaPrincipal",  regex=r'^User:[a-zA-Z0-9_@\.-]+$')
     resource_type: Resources = Field(..., alias="ResourceType")
     pattern_type: KafkaResourcePatternType = Field(..., alias="PatternType")
     resource_name: str = Field(..., alias="ResourceName", regex=r'^(\*|[a-zA-Z0-9._-]+)$')
